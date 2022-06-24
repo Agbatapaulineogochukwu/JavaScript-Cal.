6ecce270173e2fa1d221f0bc3ac2807a60d21a4a
Agbatapaulineogochukwu <105436180+Agbatapaulineogochukwu@users.noreply.github.com>
2022-06-25 00:01:52 +0100

    Add files via upload

---
+++ JavaScript cal/index.js
@@ -0,0 +1,33 @@
+
+
+
+    window.alert(firstNumber);
+    window.alert(secondNumber);
+
+
+    let firstNumber = prompt('Enter first number: ' + number1);
+    let secondNumber = prompt('Enter first number: ' + number2);
+
+    Addition == number1 + number2;
+    Subtraction == number1 - number2;
+    Multiplication == number1 * number2;
+    Division == number1 / number2;
+
+    if (Addition){
+        return number1 + number2;
+
+    } else if (Subtraction){
+        return number1 - number2;
+
+    } else if (Multiplication){
+        return number1 * number2;
+
+    } else if (Division){
+        return number1 / number2;
+
+    } else{
+        return "Inavlid";
+    
+
+}
+
